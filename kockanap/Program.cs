3f0c99cfed9e00a85e5dbac77631d05ba6f6ffca
Csabi <user@DESKTOP-MSISLED>
2017-11-17 18:47:30 +0100

    udp szerver verzió 1.0 (megj.: vacsora még nincs, Norbi nem elérhető)

--- kockanap/Program.cs
+++ kockanap/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace kockanap
@@ -10,8 +11,22 @@ namespace kockanap
     {
         static void Main(string[] args)
         {
+            // simple geometry
+            UDPServer server = new UDPServer();
+            HandleDataClass hdc = new HandleDataClass();
 
-        
+
+            Thread serverThread = new Thread(() => server.Listen());
+            serverThread.Start();
+
+            Thread dataHandlerThread = new Thread(() =>
+            hdc.SubscribeToEvent(server));
+            dataHandlerThread.Start();
+
+            while(true)
+            {
+                Thread.Sleep(100);
+            }
              
         }
     }
